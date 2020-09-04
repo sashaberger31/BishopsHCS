d0b3aa7b3ad3a35a7b298b6c394bf954801ea791
Sasha Berger <sasha@globalmilestone.org>
2020-09-03 19:23:21 -0700

    Filled in context for all locales and added dynamic support for flags.
    This was a quick update to make sure that the changes made in the database were reflected on all locales as opposed to just the US one. Also, I added context variables for the country that will dynamically switch the flags shown.

--- globalmilestone.org/frontend/pages/views.py
+++ globalmilestone.org/frontend/pages/views.py
@@ -4,9 +4,18 @@ from django.shortcuts import redirect
 from pages.models import Tutor, WebText, WebPage, WebImg, WebSection, WebInnerLink
 import sys
 # Create your views here.
+def set_locale (args, code1, code2, text1, text2):
+	args['nonlocal1'] = code1
+	args['nonlocal2'] = code2
+	args['nonlocaltext1'] = text1
+	args['nonlocaltext2'] = text2
+	args['nonlocallink1'] = 'pages/img/icons/flags/'+code1+'.png'
+	args['nonlocallink2'] = 'pages/img/icons/flags/'+code2+'.png'
+	return args
+
 
 def us(request):
-
+	# This will load the U.S. version of the homepage
 	try:
 		# Collect the content for the "landing" section
 		landing_section = WebSection.objects.get(name = "Landing")
@@ -19,10 +28,8 @@ def us(request):
 			'LandingSubtitle': landing_subtitle.text_us
 		}
 	except:
-		context = {
-			'LandingTitle' : "Contact the webmaster.",
-			'LandingSubtitle': "Contact the webmaster."
-		}
+		context['LandingTitle'] = "Contact the webmaster."
+		context['LandingSubtitle'] = "Contact the webmaster."
 
 	try:
 		# Collect the content for the "mission" section
@@ -39,17 +46,16 @@ def us(request):
 
 
 	except:
-		context = {
-			'MissionSubsectionText1' : "Contact the webmaster.",
-			'MissionSubsectionText2' : 'Contact the webmaster.',
-			'MissionSubsectionText3' : 'Contact the webmaster.',
-			'MissionSubsectionTitle1' : 'Contact the webmaster.',
-			'MissionSubsectionTitle2' : 'Contact the webmaster.',
-			'MissionSubsectionTitle3' : 'Contact the webmaster.'
-		}
+		context['MissionSubsectionText1'] = "Contact the webmaster."
+		context['MissionSubsectionTitle1'] = "Contact the webmaster."
+		context['MissionSubsectionText2'] = "Contact the webmaster."
+		context['MissionSubsectionTitle2'] = "Contact the webmaster."
+		context['MissionSubsectionText3'] = "Contact the webmaster."
+		context['MissionSubsectionTitle3'] = "Contact the webmaster."
 
 	try:
 		# Collect the content for the "how it works" section
+
 		works_section = WebSection.objects.get(name = "Works")
 		works_subsections_qset = WebSection.objects.filter(parent_section = works_section)
 		works_subsection_text = []
@@ -63,52 +69,242 @@ def us(request):
 
 
 	except:
-		context = {
-			'WorksSubsectionText1' : "Contact the webmaster.",
-			'WorksSubsectionText2' : 'Contact the webmaster.',
-			'WorksSubsectionText3' : 'Contact the webmaster.',
-			'WorksSubsectionTitle1' : 'Contact the webmaster.',
-			'WorksSubsectionTitle2' : 'Contact the webmaster.',
-			'WorksSubsectionTitle3' : 'Contact the webmaster.'
-		}
+		context['WorksSubsectionText1'] = "Contact the webmaster."
+		context['WorksSubsectionText2'] = "Contact the webmaster."
+		context['WorksSubsectionText3'] = "Contact the webmaster."
+		context['WorksSubsectionTitle1'] = "Contact the webmaster."
+		context['WorksSubsectionTitle2'] = "Contact the webmaster."
+		context['WorksSubsectionTitle3'] = "Contact the webmaster."
 
 
-	#try:
-	# Collect data for the "find out more" section.
-	more_section = WebSection.objects.get(name = "More")
-	context["MoreHeaderText"] = WebText.objects.get(parent_section = more_section).text_us
 
-	more_wrappers = []
-	for i in range(1,4):
-		more_wrappers.append(WebSection.objects.filter(parent_section = more_section).get(name = "Button"+str(i)))
+	try:
+		# Collect data for the "find out more" section.
+		more_section = WebSection.objects.get(name = "More")
+		context["MoreHeaderText"] = WebText.objects.get(parent_section = more_section).text_us
 
-	more_links = ["https://globalmilestone.org/us/" + str(WebInnerLink.objects.get(parent_section = wrapper).page.link) for wrapper in more_wrappers]
-	more_texts = [str(WebText.objects.get(parent_section = wrapper).text_us) for wrapper in more_wrappers]
+		more_wrappers = []
+		for i in range(1,4):
+			more_wrappers.append(WebSection.objects.filter(parent_section = more_section).get(name = "Button"+str(i)))
 
-	for i in range(1,4):
-		context["MoreText" + str(i)] = more_texts[i-1]
-		context["MoreLink" + str(i)] = more_links[i-1]
+		more_links = ["https://globalmilestone.org/us/" + str(WebInnerLink.objects.get(parent_section = wrapper).page.link) for wrapper in more_wrappers]
+		more_texts = [str(WebText.objects.get(parent_section = wrapper).text_us) for wrapper in more_wrappers]
 
-#except:
-	"""
-	context['MoreHeaderText'] = "Contact the webmaster."
+		for i in range(1,4):
+			context["MoreText" + str(i)] = more_texts[i-1]
+			context["MoreLink" + str(i)] = more_links[i-1]
+
+	except:
+
+		context['MoreHeaderText'] = "Contact the webmaster."
 		context['MoreText1'] = "Contact the webmaster."
-		context['MoreLink1'] = '##',
+		context['MoreLink1'] = '#',
 		context['MoreText2'] = 'Contact the webmaster.'
-		context['MoreLink2'] = '##'
+		context['MoreLink2'] = '#'
 		context['MoreText3'] = 'Contact the webmaster.'
-	context['MoreLink3'] = '##'
-	"""
+		context['MoreLink3'] = '#'
 
+	context = set_locale(context, 'mx', 'cn', "M&#233;xico", "&#20013;&#22269;")
 	response = render(request, "index.html", context)
 	response["Cache-Control"] = "no-cache, no-store, must-revalidate" # HTTP 1.1.
 	response["Pragma"] = "no-cache" # HTTP 1.0.
 	response["Expires"] = "0" # Proxies.
 	return response
 def mx(request):
-	return HttpResponse('hola')
+	# This will load the M.X. version of the homepage
+	try:
+		# Collect the content for the "landing" section
+		landing_section = WebSection.objects.get(name = "Landing")
+		landing_fields = WebText.objects.filter(parent_section = landing_section)
+		landing_title = landing_fields.get(name = "Title")
+		landing_subtitle = landing_fields.get(name = "Subtitle")
+
+		context = {
+			'LandingTitle' : landing_title.text_mx,
+			'LandingSubtitle': landing_subtitle.text_mx
+		}
+	except:
+		context['LandingTitle'] = "Contact the webmaster."
+		context['LandingSubtitle'] = "Contact the webmaster."
+
+	try:
+		# Collect the content for the "mission" section
+		mission_section = WebSection.objects.get(name = "Mission")
+		mission_subsections_qset = WebSection.objects.filter(parent_section = mission_section)
+		mission_subsection_text = []
+		mission_subsection_title = []
+		for i in range(1, 4):
+			curr_parent_section = mission_subsections_qset.get(name = ("Column"+str(i)))
+			mission_subsection_title.append(WebText.objects.filter(parent_section = curr_parent_section).get(name = "Title").text_mx)
+			mission_subsection_text.append(WebText.objects.filter(parent_section = curr_parent_section).get(name = "Text").text_mx)
+			context['MissionSubsectionText'+str(i)] = mission_subsection_text[i-1]
+			context['MissionSubsectionTitle'+str(i)] = mission_subsection_title[i-1]
+
+
+	except:
+		context['MissionSubsectionText1'] = "Contact the webmaster."
+		context['MissionSubsectionTitle1'] = "Contact the webmaster."
+		context['MissionSubsectionText2'] = "Contact the webmaster."
+		context['MissionSubsectionTitle2'] = "Contact the webmaster."
+		context['MissionSubsectionText3'] = "Contact the webmaster."
+		context['MissionSubsectionTitle3'] = "Contact the webmaster."
+
+	try:
+		# Collect the content for the "how it works" section
+
+		works_section = WebSection.objects.get(name = "Works")
+		works_subsections_qset = WebSection.objects.filter(parent_section = works_section)
+		works_subsection_text = []
+		works_subsection_title = []
+		for i in range(1, 4):
+			curr_parent_section = works_subsections_qset.get(name = ("Column"+str(i)))
+			works_subsection_title.append(WebText.objects.filter(parent_section = curr_parent_section).get(name = "Title").text_mx)
+			works_subsection_text.append(WebText.objects.filter(parent_section = curr_parent_section).get(name = "Text").text_mx)
+			context['WorksSubsectionText'+str(i)] = works_subsection_text[i-1]
+			context['WorksSubsectionTitle'+str(i)] = works_subsection_title[i-1]
+
+
+	except:
+		context['WorksSubsectionText1'] = "Contact the webmaster."
+		context['WorksSubsectionText2'] = "Contact the webmaster."
+		context['WorksSubsectionText3'] = "Contact the webmaster."
+		context['WorksSubsectionTitle1'] = "Contact the webmaster."
+		context['WorksSubsectionTitle2'] = "Contact the webmaster."
+		context['WorksSubsectionTitle3'] = "Contact the webmaster."
+
+
+
+	try:
+		# Collect data for the "find out more" section.
+		more_section = WebSection.objects.get(name = "More")
+		context["MoreHeaderText"] = WebText.objects.get(parent_section = more_section).text_mx
+
+		more_wrappers = []
+		for i in range(1,4):
+			more_wrappers.append(WebSection.objects.filter(parent_section = more_section).get(name = "Button"+str(i)))
+
+		more_links = ["https://globalmilestone.org/mx/" + str(WebInnerLink.objects.get(parent_section = wrapper).page.link) for wrapper in more_wrappers]
+		more_texts = [str(WebText.objects.get(parent_section = wrapper).text_mx) for wrapper in more_wrappers]
+
+		for i in range(1,4):
+			context["MoreText" + str(i)] = more_texts[i-1]
+			context["MoreLink" + str(i)] = more_links[i-1]
+
+	except:
+
+		context['MoreHeaderText'] = "Contact the webmaster."
+		context['MoreText1'] = "Contact the webmaster."
+		context['MoreLink1'] = '#',
+		context['MoreText2'] = 'Contact the webmaster.'
+		context['MoreLink2'] = '#'
+		context['MoreText3'] = 'Contact the webmaster.'
+		context['MoreLink3'] = '#'
+
+	context = set_locale(context, 'us', 'cn', "US", "&#20013;&#22269;")
+
+	response = render(request, "index.html", context)
+	response["Cache-Control"] = "no-cache, no-store, must-revalidate" # HTTP 1.1.
+	response["Pragma"] = "no-cache" # HTTP 1.0.
+	response["Expires"] = "0" # Proxies.
+	return response
+
 def cn(request):
-	return HttpResponse('ni hao')
+	# This will load the U.S. version of the homepage
+	try:
+		# Collect the content for the "landing" section
+		landing_section = WebSection.objects.get(name = "Landing")
+		landing_fields = WebText.objects.filter(parent_section = landing_section)
+		landing_title = landing_fields.get(name = "Title")
+		landing_subtitle = landing_fields.get(name = "Subtitle")
+
+		context = {
+			'LandingTitle' : landing_title.text_cn,
+			'LandingSubtitle': landing_subtitle.text_cn,
+		}
+	except:
+		context['LandingTitle'] = "Contact the webmaster."
+		context['LandingSubtitle'] = "Contact the webmaster."
+
+	try:
+		# Collect the content for the "mission" section
+		mission_section = WebSection.objects.get(name = "Mission")
+		mission_subsections_qset = WebSection.objects.filter(parent_section = mission_section)
+		mission_subsection_text = []
+		mission_subsection_title = []
+		for i in range(1, 4):
+			curr_parent_section = mission_subsections_qset.get(name = ("Column"+str(i)))
+			mission_subsection_title.append(WebText.objects.filter(parent_section = curr_parent_section).get(name = "Title").text_cn)
+			mission_subsection_text.append(WebText.objects.filter(parent_section = curr_parent_section).get(name = "Text").text_cn)
+			context['MissionSubsectionText'+str(i)] = mission_subsection_text[i-1]
+			context['MissionSubsectionTitle'+str(i)] = mission_subsection_title[i-1]
+
+
+	except:
+		context['MissionSubsectionText1'] = "Contact the webmaster."
+		context['MissionSubsectionTitle1'] = "Contact the webmaster."
+		context['MissionSubsectionText2'] = "Contact the webmaster."
+		context['MissionSubsectionTitle2'] = "Contact the webmaster."
+		context['MissionSubsectionText3'] = "Contact the webmaster."
+		context['MissionSubsectionTitle3'] = "Contact the webmaster."
+
+	try:
+		# Collect the content for the "how it works" section
+
+		works_section = WebSection.objects.get(name = "Works")
+		works_subsections_qset = WebSection.objects.filter(parent_section = works_section)
+		works_subsection_text = []
+		works_subsection_title = []
+		for i in range(1, 4):
+			curr_parent_section = works_subsections_qset.get(name = ("Column"+str(i)))
+			works_subsection_title.append(WebText.objects.filter(parent_section = curr_parent_section).get(name = "Title").text_cn)
+			works_subsection_text.append(WebText.objects.filter(parent_section = curr_parent_section).get(name = "Text").text_cn)
+			context['WorksSubsectionText'+str(i)] = works_subsection_text[i-1]
+			context['WorksSubsectionTitle'+str(i)] = works_subsection_title[i-1]
+
+
+	except:
+		context['WorksSubsectionText1'] = "Contact the webmaster."
+		context['WorksSubsectionText2'] = "Contact the webmaster."
+		context['WorksSubsectionText3'] = "Contact the webmaster."
+		context['WorksSubsectionTitle1'] = "Contact the webmaster."
+		context['WorksSubsectionTitle2'] = "Contact the webmaster."
+		context['WorksSubsectionTitle3'] = "Contact the webmaster."
+
+
+
+	try:
+		# Collect data for the "find out more" section.
+		more_section = WebSection.objects.get(name = "More")
+		context["MoreHeaderText"] = WebText.objects.get(parent_section = more_section).text_cn
+
+		more_wrappers = []
+		for i in range(1,4):
+			more_wrappers.append(WebSection.objects.filter(parent_section = more_section).get(name = "Button"+str(i)))
+
+		more_links = ["https://globalmilestone.org/cn/" + str(WebInnerLink.objects.get(parent_section = wrapper).page.link) for wrapper in more_wrappers]
+		more_texts = [str(WebText.objects.get(parent_section = wrapper).text_cn) for wrapper in more_wrappers]
+
+		for i in range(1,4):
+			context["MoreText" + str(i)] = more_texts[i-1]
+			context["MoreLink" + str(i)] = more_links[i-1]
+
+	except:
+
+		context['MoreHeaderText'] = "Contact the webmaster."
+		context['MoreText1'] = "Contact the webmaster."
+		context['MoreLink1'] = '#',
+		context['MoreText2'] = 'Contact the webmaster.'
+		context['MoreLink2'] = '#'
+		context['MoreText3'] = 'Contact the webmaster.'
+		context['MoreLink3'] = '#'
+
+	context = set_locale(context, 'mx', 'us', "M&#233;xico", "US")
+
+	response = render(request, "index.html", context)
+	response["Cache-Control"] = "no-cache, no-store, must-revalidate" # HTTP 1.1.
+	response["Pragma"] = "no-cache" # HTTP 1.0.
+	response["Expires"] = "0" # Proxies.
+	return response
 
 def enroll_us(request):
 
