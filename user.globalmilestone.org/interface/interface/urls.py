682aa48d5e12fcf4bcba9abf6732e020e06f9801
Sasha Berger <sasha@globalmilestone.org>
2020-09-21 23:57:22 -0700

    Added basic template for the login page.

--- user.globalmilestone.org/interface/interface/urls.py
+++ user.globalmilestone.org/interface/interface/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('standard/', include('standard.urls')),
+    path('/', include('standard.urls')),
 ]
